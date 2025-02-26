d2d468e426f5aedd6113478f3911c5a4544f58e7
Delaram Rohani <dlrmrhi80@gmail.com>
2025-02-26 17:33:04 +0330

    Change pages/... to Component/...

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+import Login from "./Component/Login";
+import Signup from "./Component/Signup";
+import Home from "./Component/Home";
 import './App.css'
 
 function App() {
