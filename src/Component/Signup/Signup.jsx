f6b22d72fc30b3dca28807094a8fbe5752be9473
Delaram Rohani <dlrmrhi80@gmail.com>
2025-03-01 03:18:08 +0330

    Change the styles

---
+++ src/Component/Signup/Signup.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { TextField, Button, Container, Typography, Box, Alert } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import './signup.css'
+
+const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      await axios.post("http://localhost:3000/api/auth/register", {
+        name,
+        email,
+        password,
+      });
+
+      navigate("/login");
+    } catch (err) {
+      setError(err.response?.data?.message || "Registration failed!");
+    }
+  };
+
+  return (
+    <Container class="container">
+      <Box>
+        <Typography class="title">Register</Typography>
+      </Box>
+
+      {error && <Alert class="error-message">{error}</Alert>}
+
+      <form onSubmit={handleRegister}>
+        <TextField
+          className="input-field"
+          label="Full Name"
+          type="text"
+          variant="outlined"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <TextField
+          className="input-field"
+          label="Email"
+          type="email"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <TextField
+          className="input-field"
+          label="Password"
+          type="password"
+          variant="outlined"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <Button class="register-button" type="submit">
+          Register
+        </Button>
+      </form>
+
+      <Box class="login-link">
+        <Typography variant="body2">
+          Already have an account?{" "}
+          <Button onClick={() => navigate("/login")}>Login</Button>
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default Signup;
